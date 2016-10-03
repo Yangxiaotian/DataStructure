79c27491d83ad9cb452fbd217b4b732e01ee59e4
杨笑天 <yangxiaotian@yangxiaotiandeiMac.local>
2016-10-03 18:05:51 +0800

    Initial Commit

---
+++ DataStructure/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure
+//
+//  Created by 杨笑天 on 2016/10/3.
+//  Copyright © 2016年 杨笑天. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
