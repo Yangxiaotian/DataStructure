c63a169da31a8c7ac59041cd65ece5f11389a5f4
Yangxiaotian <stdlib@foxmail.com>
2016-10-10 20:45:19 +0800

    update LinkList

--- DataStructure/LinkList.c
+++ DataStructure/LinkList.c
@@ -3,7 +3,7 @@
 //  DataStructure
 //
 //  Created by 杨笑天 on 2016/10/5.
-//  Copyright © 2016年 杨笑天. All rights reserved.
+//  Copyright  2016年 杨笑天. All rights reserved.
 //
 
 #include "DataStructure.h"
@@ -12,8 +12,9 @@ typedef struct LNode {
     struct LNode *next;
 }LNode, *LinkList;
 //初始化链表
-Status InitList_lk(LinkList L) {
-    L->next = NULL;
+Status InitList_lk(LinkList *L) {
+	*L = (LinkList)malloc(sizeof(LNode)); 
+    (*L)->next = NULL;
     return OK;
 }
 //获取第i个元素
@@ -38,8 +39,8 @@ Status GetElem_lk(LinkList L, int i, ElemType *e) {
     return OK;
 }
 //从第i个位置插入元素
-Status InsertList_lk(LinkList L, int i, ElemType e) {
-    LinkList p = L;
+Status InsertList_lk(LinkList *L, int i, ElemType e) {
+    LinkList p = *L;
     int j = 0;
     //此while循环最多运行i-1次，（如往第1个位置插入元素，不进入循环）
     while(p && j < i-1) {
@@ -55,8 +56,8 @@ Status InsertList_lk(LinkList L, int i, ElemType e) {
     return OK;
 }
 //删除第i个元素
-Status DeleteList_lk(LinkList L, int i, ElemType *e){
-    LinkList p=L;
+Status DeleteList_lk(LinkList *L, int i, ElemType *e){
+    LinkList p=*L;
     int j=0;
     while(p&&j<i-1){
         p=p->next;
@@ -64,8 +65,35 @@ Status DeleteList_lk(LinkList L, int i, ElemType *e){
     }
     if(!p->next || j>i-1) return ERROR;
     Copy(e, &(p->next->data));
-    free(p->next->data);
+    free(p->next);
     p->next=p->next->next;
     return OK;
+}
+//打印链表
+Status PrintList_lk(LinkList L) {
+	LinkList p = L->next;
+	while(p) {
+		PrintElem(p->data);
+		p = p->next;
+	}
+} 
+int main() {
+	//初始化
+	LinkList L;
+	InitList_lk(&L);
+	ElemType e;
+    //插入测试
+    printf("插入测试：\n---------------------\n");
+    printf("输入名称\t数量\t价格\t备注：");
+    scanf("%s%d%f%s", e.name, &e.num, &e.price, e.remark);
+    InsertList_lk(&L, 1, e);
+    PrintList_lk(L);
+    //删除测试
+	printf("删除测试：\n---------------------\n");
+    ElemType del;
+    DeleteList_lk(&L, 1, &del);
+    PrintElem(del); 
+    PrintList_lk(L);
+	return 0; 
 }
 
